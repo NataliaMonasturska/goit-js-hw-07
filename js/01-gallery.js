bb5e90f9b1479a9aea066ee3738c9d7c78567c68
Natalia29011989 <tjomamon@gmail.com>
2022-05-16 00:09:29 +0300

    1.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
+import * as basicLightbox from "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js"
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector("div.gallery")
+
+
+const GalleryItems = galleryItems.map((item) => 
+    
+    `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>
+</div>`)
+  .join("");
+    
+    gallery.insertAdjacentHTML("afterbegin", GalleryItems);
+   
+
+const lightbox = galleryItems.map((item) => 
+  `<img 
+  src="${item.original}"
+   width="800"
+   height="600"></img>`).join(""); 
+
+
+// gallery.onclick = () => {
+
+//   basicLightbox.create(
+
+//   `${lightbox}`
+// ).show()
+// }
+// gallery.addEventListener("click", instance.show());
+
+
+
+
+const instance = basicLightbox.create(
+  `${lightbox}`
+)
+
+instance.show()
+
+
