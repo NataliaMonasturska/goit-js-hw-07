62330c1c249818216c834e5970d9991462fd1e6e
Natalia29011989 <tjomamon@gmail.com>
2022-05-16 18:39:07 +0300

    1 задание полностью готово

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,5 @@
 import { galleryItems } from './gallery-items.js';
-// import * as basicLightbox from "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js"
-// Change code below this line
-// console.log(galleryItems);
+
 const gallery = document.querySelector("div.gallery")
 const GalleryItems = galleryItems.map((item) => 
     
@@ -22,19 +20,24 @@ gallery.insertAdjacentHTML("afterbegin", GalleryItems);
 
 gallery.addEventListener("click", (event) => {
   event.preventDefault();
-   if (event.target.nodeName !== "IMG") {
+  if (event.target.nodeName !== "IMG") {
     return;
-   }
+  }
   const selectedColor = event.target.dataset.source;
 
   const instance = basicLightbox.create(
-  `<img 
+    `<img 
    src= "${selectedColor}"
     width="800"
     height="600"></img>`
-  )
-instance.show()
- 
- } )
+     
+  );
+  instance.show(window.addEventListener('keydown',(event)=>{
+  if (event.code === 'Escape') {
+      instance.close()
+  } 
+}));
+});
 
- 
+
+
